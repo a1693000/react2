4856a4afbf017749783b2b4171d8317c964ee05c
Erick Zen Iwamoto <erick_zen_@hotmail.com>
2019-03-20 14:55:39 -0300

    aula tester, incompleto

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,11 @@ import logo from '../assets/logo.jpg';
 const Main = () => (
   <div>
     <img src={logo} alt="Github Compare" />
+
+    <form>
+      <input type="text" placeholder="usuário/repositório" />
+      <button type="submit">OK</button>
+    </form>
   </div>
 );
 
